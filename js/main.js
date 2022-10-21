3812f1ae8df527fc6fa2d5c1738a6c471fe6fd3c
MccGrey <chuxzzie01@yahoo.com>
2022-10-21 12:48:22 +0100

    add functionality for remove item from cart

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
   cart.classList.remove("active");
 };
+
+// cart working js
+
+if (document.readState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+//making function
+
+function ready() {
+  //remover items from cart
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButtons);
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+}
+
+//remover items from cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+}
+
+//
